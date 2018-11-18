a6c66fb6ba1c82fc19ce4c24cbfeefc7a9c87b00
BEJIDE Daniel Oluwole <digitiqman@gmail.com>
2018-11-18 20:45:31 +0100

    Added bad account request

---
+++ AutoBank/Models/BadAccountRequest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Autobank.Models
+{
+
+    public class BadAccountRequest
+    {
+        [Required]
+        public string AccountNumber { get; set; }
+
+        [Required]
+        public string Amount { get; set; }
+
+        [Required]
+        public int Currency { get; set; }
+    }
+}
